0734aec6057e86fbc5befa6a95a173106f61784b
Andre Haikal <andre.haikal@veriff.net>
2019-10-26 23:01:24 +0300

    fix form

--- src/components/form.js
+++ src/components/form.js
@@ -1,44 +1,84 @@
-import React from "react"
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from 'axios';
+
+
+const LoginSchema = Yup.object().shape({
+  appartmentSize: Yup
+    .string()
+    .required("appartment Size is required"),
+  postcode: Yup.string()
+    .min(3, "postcode must be 3 characters at minimum")
+    .required("postcode is required")
+});
+
 export default class IndexPage extends React.Component {
-  state = {
-    apartmentSize: "",
-    postCode: "",
-  }
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    this.setState({
-      [name]: value,
-    })
-  }
-  handleSubmit = event => {
-    event.preventDefault()
-    alert(`Welcome ${this.state.apartmentSize} ${this.state.postCode}!`)
-  }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Home Size
-          <input
-            type="text"
-            name="apartmentSize"
-            value={this.state.apartmentSize}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Post Code
-          <input
-            type="text"
-            name="postCode"
-            value={this.state.postCode}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    )
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-lg-7">
+            <Formik
+              initialValues={{ appartmentSize: "", postcode: "" }}
+              validationSchema={LoginSchema}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              {({ touched, errors, isSubmitting }) => (
+                <Form>
+                  <div className="row">
+                    <div className="col">
+                    <Field
+                    component="select"
+                    className="form-control"
+                    name="appartmentSize"
+                    >
+                      <option value="" disabled="">Home size</option>
+                      <option value="2">&lt; 40m²</option>
+                      <option value="2.5">40 - 49m²</option>
+                      <option value="3">50 - 59m²</option>
+                      <option value="3.5">60 - 69m²</option>
+                      <option value="4">70 - 84m²</option>
+                      <option value="4.5">85 - 99m²</option>
+                      <option value="5">100 - 114m²</option>
+                      <option value="5.5">115 - 129m²</option>
+                      <option value="6">130 - 144m²</option>
+                      <option value="6.5">145 - 159m²</option>
+                      <option value="7">160 - 174m²</option>
+                      <option value="7.5">175 - 190m²</option>
+                      <option value="8">&gt; 190m²</option>
+                    </Field>
+                  </div>
+                  <div className="col">
+                    <Field
+                      type="postcode"
+                      name="postcode"
+                      placeholder="Enter postcode"
+                      className={`form-control ${
+                        touched.postcode && errors.postcode ? "is-invalid" : ""
+                      }`}
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-block col"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? "Please wait..." : "Submit"}
+                  </button>
+                </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import Layout from "../components/layout"
 import Form from "../components/form"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
       <Image style={{ postion: `absolute`, maxHeight: `50%`, left: `0`, top: `0`, width: `100%`, height: `100%` }}/>
-      <Form style={{ postion: `absolute`,  left: `0.5`, top: `1`, index: `2`}}/>
+      <Form />
   </Layout>
 )
 
