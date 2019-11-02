16a384b8944b534b604560be2ff270a8951fd08e
Andre Haikal <andre.haikal@inbank.ee>
2019-11-02 22:40:47 +0200

    add axios post and titles

--- src/components/form.js
+++ src/components/form.js
@@ -13,72 +13,77 @@ const LoginSchema = Yup.object().shape({
     .required("postcode is required")
 });
 
+const instance = axios.create({
+  baseURL: 'https://api.cozee.ee',
+  headers: {'Content-Type:': 'application/json; charset=utf-8'}
+});
+
 export default class IndexPage extends React.Component {
   render() {
     return (
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-lg-7">
-            <Formik
-              initialValues={{ appartmentSize: "", postcode: "" }}
-              validationSchema={LoginSchema}
-              onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2));
-                  setSubmitting(false);
-                }, 400);
-              }}
-            >
-              {({ touched, errors, isSubmitting }) => (
-                <Form>
-                  <div className="row">
-                    <div className="col">
-                    <Field
-                    component="select"
-                    className="form-control"
-                    name="appartmentSize"
-                    >
-                      <option value="" disabled="">Home size</option>
-                      <option value="2">&lt; 40m²</option>
-                      <option value="2.5">40 - 49m²</option>
-                      <option value="3">50 - 59m²</option>
-                      <option value="3.5">60 - 69m²</option>
-                      <option value="4">70 - 84m²</option>
-                      <option value="4.5">85 - 99m²</option>
-                      <option value="5">100 - 114m²</option>
-                      <option value="5.5">115 - 129m²</option>
-                      <option value="6">130 - 144m²</option>
-                      <option value="6.5">145 - 159m²</option>
-                      <option value="7">160 - 174m²</option>
-                      <option value="7.5">175 - 190m²</option>
-                      <option value="8">&gt; 190m²</option>
-                    </Field>
-                  </div>
-                  <div className="col">
-                    <Field
-                      type="postcode"
-                      name="postcode"
-                      placeholder="Enter postcode"
-                      className={`form-control ${
-                        touched.postcode && errors.postcode ? "is-invalid" : ""
-                      }`}
-                    />
-                  </div>
+      <Formik
+        initialValues={{ appartmentSize: "", postcode: "" }}
+        validationSchema={LoginSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          axios.post('/bookings', {
+            duration: values.appartmentSize,
+            zip_code: values.postcode
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        }}
+      >
+        {({ touched, errors, isSubmitting }) => (
+          <Form>
+            <div className="row">
+              <div className="col">
+              <Field
+              component="select"
+              className="form-control"
+              name="appartmentSize"
+              >
+                <option value="" disabled="">Home size</option>
+                <option value="2">&lt; 40m²</option>
+                <option value="2.5">40 - 49m²</option>
+                <option value="3">50 - 59m²</option>
+                <option value="3.5">60 - 69m²</option>
+                <option value="4">70 - 84m²</option>
+                <option value="4.5">85 - 99m²</option>
+                <option value="5">100 - 114m²</option>
+                <option value="5.5">115 - 129m²</option>
+                <option value="6">130 - 144m²</option>
+                <option value="6.5">145 - 159m²</option>
+                <option value="7">160 - 174m²</option>
+                <option value="7.5">175 - 190m²</option>
+                <option value="8">&gt; 190m²</option>
+              </Field>
+            </div>
+            <div className="col">
+              <Field
+                type="postcode"
+                name="postcode"
+                placeholder="Enter postcode"
+                className={`form-control ${
+                  touched.postcode && errors.postcode ? "is-invalid" : ""
+                }`}
+              />
+            </div>
 
-                  <button
-                    type="submit"
-                    className="btn btn-primary btn-block col"
-                    disabled={isSubmitting}
-                  >
-                    {isSubmitting ? "Please wait..." : "Submit"}
-                  </button>
-                </div>
-                </Form>
-              )}
-            </Formik>
+            <button
+              type="submit"
+              className="btn btn-primary btn-block col"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Please wait..." : "Submit"}
+            </button>
           </div>
-        </div>
-      </div>
+          </Form>
+        )}
+      </Formik>
     );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,17 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
       <Image style={{ postion: `absolute`, maxHeight: `50%`, left: `0`, top: `0`, width: `100%`, height: `100%` }}/>
-      <Form />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-lg-7" >
+            <div style={{ color: `white`}}>
+              <h6>Happy to be home</h6>
+              <h1>Cleaning made simple!</h1>
+            </div>
+            <Form />
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
