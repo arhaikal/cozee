911638e9941510025064a5003b2e73403433b442
Andre Haikal <andre.haikal@inbank.ee>
2020-01-23 19:13:10 +0200

    test build

--- src/components/duration-selector.js
+++ src/components/duration-selector.js
@@ -5,7 +5,6 @@ import { BookingContext } from '../context/BookingContext';
 import { updateBooking } from '../actions/index'
 import { TIME } from './data';
 import { Select, Heading, Box, RadioButtonGroup, SimpleGrid } from '@chakra-ui/core';
-import { CustomRadio } from './custom-radio';
 
 export const DurationSelector = () => {
   const [state, dispatch] = useContext(BookingContext);
@@ -17,20 +16,7 @@ export const DurationSelector = () => {
     <Box witdh="100%" rounded="lg" className="card-big">
       <Box>
         <Heading as="h3" size="lg" mb="5">How many hours should we clean?</Heading>
-        <RadioButtonGroup
-          defaultValue={state.booking.duration}
-          onChange={updateDuration}
-          name="duration"
-          isInline
-          spacing='22px'
-        >
-          {TIME.map((obj, key) =>
-            <CustomRadio
-              w={100} mb={5}
-              value={obj.hours}
-              key={key}>{`${obj.hours}`}
-            </CustomRadio>)}
-        </RadioButtonGroup>
+
       </Box>
     </Box >
   );
