f22b5c60eb446f2eaaab4c22aad3673b42d14897
Andre Haikal <andre.haikal@inbank.ee>
2020-01-23 20:40:18 +0200

    update calendar

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { Box, Flex, Text, Button, Heading } from '@chakra-ui/core'
+import { CustomRadio } from './custom-radio';
 
 export const Calendar = () => {
   return (
@@ -55,6 +56,131 @@ export const Calendar = () => {
             <Text fontSize="lg">at 17:00</Text>
           </Flex>
         </Button>
+      </Flex>
+      <Flex justify="space-between" mt={8}>
+        <Box>
+          <Box>
+            <Heading as="h6" size="xs">
+              Monday
+          </Heading>
+            <Text fontSize="sm">
+              23. feb
+          </Text>
+          </Box>
+          <Box>
+            <Flex direction='column' justify="space-between">
+              <CustomRadio mt={4}>
+                14:00
+            </CustomRadio>
+              <CustomRadio mt={2}>
+                15:00
+            </CustomRadio>
+            </Flex>
+          </Box>
+        </Box>
+        <Box>
+          <Heading as="h6" size="xs">
+            Tuesday
+          </Heading>
+          <Text fontSize="sm">
+            24. feb
+          </Text>
+          <Box>
+            <Flex direction='column' justify="space-between">
+              <CustomRadio mt={4}>
+                7:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                9:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                13:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                17:00
+              </CustomRadio>
+            </Flex>
+          </Box>
+        </Box>
+        <Box>
+          <Heading as="h6" size="xs">
+            Wednesday
+          </Heading>
+          <Text fontSize="sm">
+            25. feb
+          </Text>
+          <Box>
+            <Flex direction='column' justify="space-between">
+              <CustomRadio mt={4}>
+                7:00
+              </CustomRadio>
+            </Flex>
+          </Box>
+        </Box>
+        <Box>
+          <Heading as="h6" size="xs">
+            Thursday
+          </Heading>
+          <Text fontSize="sm">
+            26. feb
+          </Text>
+          <Box>
+            <Flex direction='column' justify="space-between">
+              <CustomRadio mt={4}>
+                7:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                9:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                13:00
+              </CustomRadio>
+            </Flex>
+          </Box>
+        </Box>
+        <Box>
+          <Heading as="h6" size="xs">
+            Friday
+          </Heading>
+          <Text fontSize="sm">
+            27. feb
+          </Text>
+        </Box>
+        <Box>
+          <Heading as="h6" size="xs">
+            Saturday
+          </Heading>
+          <Text fontSize="sm">
+            28. feb
+          </Text>
+          <Box>
+            <Flex direction='column' justify="space-between">
+              <CustomRadio mt={4}>
+                7:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                9:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                13:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                17:00
+              </CustomRadio>
+              <CustomRadio mt={2}>
+                19:00
+              </CustomRadio>
+            </Flex>
+          </Box>
+        </Box>
+        <Box>
+          <Heading as="h6" size="xs">
+            Sunday
+          </Heading>
+          <Text fontSize="sm">
+            29. feb
+          </Text>
+        </Box>
       </Flex>
     </Box>
   );
