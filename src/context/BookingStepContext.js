92e8bc9e0564daa234703abc69e587c5af4bf6b8
Andre Haikal <andre.haikal@veriff.net>
2020-05-03 00:15:26 +0300

    fix bug

--- src/context/BookingStepContext.js
+++ src/context/BookingStepContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from "react"
 export const BookingStepContext = createContext()
 
 export const BookingStepProvider = props => {
-  const [state, dispatch] = useState(1)
+  const [state, dispatch] = useState({ step: 1 })
 
   return (
     <BookingStepContext.Provider value={[state, dispatch]}>
